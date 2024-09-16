8f2292d64ce447acc98ea6c2ad5c8f4af44dfc37
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-16 16:34:29 +0300

    AlphabetSoup algorithm solved.

---
+++ Algorithms/AlphabetSoup/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AlphabetSoup
+{
+	// Ensure that the function takes the provided string parameter and returns the string with its letters sorted in alphabetical order.
+	// Assume that numbers and punctuation marks are not included in the processing.
+
+	// Example Input: Console.WriteLine(AlphabetSoup("coderbyte"));
+	//        Output: bcdeeorty
+
+	// Example Input: Console.WriteLine(AlphabetSoup("hooplah"));
+	//        Output: ahhloop
+
+	internal class Program
+	{
+		private static string AlphabetSoup(string data)
+		{
+			string result = "";
+			char[] chars = data.ToCharArray();
+			Array.Sort(chars);
+			foreach (char c in chars)
+			{
+				result += c;
+			}
+			return result;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(AlphabetSoup("coderbyte"));
+			Console.WriteLine(AlphabetSoup("hooplah"));
+		}
+	}
+}
