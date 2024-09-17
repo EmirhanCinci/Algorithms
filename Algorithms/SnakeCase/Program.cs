19cc31034e19323b75fb7d585f40f2f1d8829141
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-17 08:12:55 +0300

    SnakeCase algorithm solved.

---
+++ Algorithms/SnakeCase/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace SnakeCase
+{
+	// Write a function that returns the input string in snake_case format,
+	// where each word is written in lowercase and words are separated by underscores.
+	// The string will only contain letters and a few punctuation marks as delimiters between words.
+
+	// Example Input: Console.WriteLine(SnakeCase("cats AND*Dogs-are Awesome"));
+	// Example Output: cats_and_dogs_are_awesome
+
+	// Example Input: Console.WriteLine(SnakeCase("a b c d-e-f%g"));
+	// Example Output: a_b_c_d_e_f_g
+
+	// Example Input: Console.WriteLine(SnakeCase("BOB loves-coding"));
+	// Example Output: bob_loves_coding
+
+	internal class Program
+	{
+		private static string SnakeCase(string data)
+		{
+			string result = "";
+			data = data.ToLower();
+			foreach (char c in data)
+			{
+				if (c >= 97 && c <= 122)
+				{
+					result += c;
+				}
+				else
+				{
+					result += "_";
+				}
+			}
+			return result;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(SnakeCase("cats AND*Dogs-are Awesome"));
+			Console.WriteLine(SnakeCase("a b c d-e-f%g"));
+			Console.WriteLine(SnakeCase("BOB loves-coding"));
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Algorithms
  These are algorithm questions I solved with C#.
+
+1. [FizzBuzz](https://github.com/EmirhanCinci/Algorithms/blob/main/Algorithms/FizzBuzz/Program.cs)
+2. [BitwiseTwo](https://github.com/EmirhanCinci/Algorithms/blob/main/Algorithms/BitwiseTwo/Program.cs)
+3. [SuperIncreasing](https://github.com/EmirhanCinci/Algorithms/blob/main/Algorithms/SuperIncreasing/Program.cs)
+4. [AlphabetSearching](https://github.com/EmirhanCinci/Algorithms/blob/main/Algorithms/AlphabetSearching/Program.cs)
+5. [AlphabetSoup](https://github.com/EmirhanCinci/Algorithms/blob/main/Algorithms/AlphabetSoup/Program.cs)
+6. [SnakeCase](https://github.com/EmirhanCinci/Algorithms/blob/main/Algorithms/SnakeCase/Program.cs)
